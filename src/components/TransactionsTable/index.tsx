da28c15812d0397bda6a427ea9c1443b21d35407
henriqueberaldo11 <henriqueberaldo11@gmail.com>
2021-03-18 14:49:00 -0300

    projeto dtMoney

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+interface Trasaction {
+  id: number,
+  title: string,
+  type: string,
+  category: string,
+  amount: number,
+  createdAt: string  
+}
+
+export function TransactionsTable () {
+  const [transactions, setTransactions] = useState<Trasaction[]>([]);
+
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => setTransactions(response.data.transactions))
+  }, [])
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map( transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  )
+}
